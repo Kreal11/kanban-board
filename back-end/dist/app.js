f5330ca26a1d1e8ce3c6c837722f77235a876fa4
Kreal11 <kirillshyrokov2@gmail.com>
2024-03-15 23:49:04 +0200

    created a project, added FE and BE + dbHost

---
+++ back-end/dist/app.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+require("dotenv/config");
+const app = (0, express_1.default)();
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+app.use((0, morgan_1.default)(formatsLogger));
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.static('public'));
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' });
+});
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Server error' } = err;
+    res.status(status).json({ message, err });
+});
+exports.default = app;
---
+++ back-end/src/conf/envConfs.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+const {
+    DB_HOST,
+    PORT,
+    BASE_URL,
+    SECRET_KEY,
+} = process.env;
+
+interface EnvsConfig {
+    port?: string;
+    dbHost?: string;
+    baseUrl?: string;
+    secretKey?: string;
+}
+
+const envsConfig: EnvsConfig = {
+    port: PORT,
+    dbHost: DB_HOST,
+    baseUrl: BASE_URL,
+    secretKey: SECRET_KEY,
+};
+
+export default envsConfig;
