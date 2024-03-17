6ca47bdaed4c6935ec30d413b8aa56f45184c7db
Kreal11 <kirillshyrokov2@gmail.com>
2024-03-17 20:52:18 +0200

    added markup for home page

--- front-end/src/components/boardItem/BoardItem.tsx
+++ front-end/src/components/boardItem/BoardItem.tsx
@@ -1,5 +1,18 @@
-const BoardItem = () => {
-  return <div>BoardItem</div>;
+interface BoardItemProps {
+  title?: string;
+  theme?: string;
+  _id?: string;
+}
+
+const BoardItem = ({ title, theme, _id }: BoardItemProps) => {
+  return (
+    <>
+      <li>
+        <h2>{title}</h2>
+        <p>{theme}</p>
+      </li>
+    </>
+  );
 };
 
 export default BoardItem;
--- front-end/src/pages/home/Home.tsx
+++ front-end/src/pages/home/Home.tsx
@@ -3,6 +3,8 @@ import { selectGetBoards } from "../../redux/board/selectors";
 import { useEffect } from "react";
 import { useAppDispatch } from "../../redux/hooks";
 import { getAllBoardsThunk } from "../../redux/board/operations";
+import BoardItem from "../../components/boardItem/BoardItem";
+import { BoardsWrapper } from "./Home.styled";
 
 const Home = () => {
   const boards = useSelector(selectGetBoards);
@@ -13,10 +15,15 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      {boards?.length &&
-        boards.map((board) => <li key={board._id}>{board.title}</li>)}
-    </div>
+    <BoardsWrapper>
+      <div>
+        <p>Add board</p>
+      </div>
+      <ul>
+        {boards?.length &&
+          boards.map((board) => <BoardItem key={board._id} {...board} />)}
+      </ul>
+    </BoardsWrapper>
   );
 };
 
